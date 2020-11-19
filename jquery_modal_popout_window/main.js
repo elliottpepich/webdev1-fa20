183f590ebf83463a9f0f1e0afe2abaff673e8d9e
elliottpepich <71103363+elliottpepich@users.noreply.github.com>
2020-11-19 13:16:28 -0600

    test1

---
+++ jquery_modal_popout_window/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(".button").click(function() {
+        $("#myform #valueFromMyButton").text($(this).val().trim());
+        $("#myform input[type=text]").val('');
+        $("#myform").show(500);
+    });
+    $("#btnOK").click(function() {
+        $("#valueFromMyModal").val($("#myform input[type=text]").val().trim());
+        $("#myform").hide(400);
+    });
+});
