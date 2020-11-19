80cea2bc20f12c312967a3268f06a83f5474f8fc
elliottpepich <71103363+elliottpepich@users.noreply.github.com>
2020-11-19 15:27:03 -0600

    test3

--- jquery_modal_popout_window/main.js
+++ jquery_modal_popout_window/main.js
@@ -1,29 +1,7 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+$(".open").on("click", function() {
+  $(".popup-overlay, .popup-content").addClass("active");
+});
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModa(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+$(".close, .popup-overlay").on("click", function() {
+  $(".popup-overlay, .popup-content").removeClass("active");
+});
